a0f0932a2a034e698369bca11a2546e0f381c802
Анна <ann1974chernova@gmail.com>
2024-03-29 01:50:06 +0300

    style: check

--- scrapper/src/main/java/edu/java/domain/TgChat.java
+++ scrapper/src/main/java/edu/java/domain/TgChat.java
@@ -1,19 +1,11 @@
 package edu.java.domain;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-import java.util.HashSet;
-import java.util.Set;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Table(name = "tg_chats")
 @Entity
@@ -30,9 +22,9 @@ public class TgChat {
 
     @ManyToMany(cascade = {CascadeType.ALL})
     @JoinTable(
-        name = "tg_chat_links",
-        joinColumns = {@JoinColumn(name = "tg_chat_id")},
-        inverseJoinColumns = {@JoinColumn(name = "link_id")}
+            name = "tg_chat_links",
+            joinColumns = {@JoinColumn(name = "tg_chat_id")},
+            inverseJoinColumns = {@JoinColumn(name = "link_id")}
     )
     private Set<Link> links = new HashSet<>();
 
--- scrapper/src/main/java/edu/java/repository/jdbc/JdbcLinkRepository.java
+++ scrapper/src/main/java/edu/java/repository/jdbc/JdbcLinkRepository.java
@@ -4,16 +4,18 @@ import edu.java.domain.Link;
 import edu.java.domain.TgChat;
 import edu.java.repository.LinkRepository;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import java.net.URI;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import lombok.AllArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+
 import static edu.java.repository.jdbc.JdbcMapper.listMapToSetLink;
 import static edu.java.repository.jdbc.JdbcMapper.listMapToSetTgChat;
 
@@ -28,10 +30,10 @@ public class JdbcLinkRepository implements LinkRepository {
         Optional<Long> linkId = findIdByUri(link.getUri());
         if (linkId.isPresent()) {
             Long count = jdbcTemplate.queryForObject(
-                "select count(*) from tg_chat_links where tg_chat_id = ? and link_id = ?",
-                Long.class,
-                tgChatId,
-                linkId.get()
+                    "select count(*) from tg_chat_links where tg_chat_id = ? and link_id = ?",
+                    Long.class,
+                    tgChatId,
+                    linkId.get()
             );
             if (count != 0L) {
                 return 0;
@@ -39,18 +41,20 @@ public class JdbcLinkRepository implements LinkRepository {
         }
         if (linkId.isEmpty()) {
             jdbcTemplate.update(
-                "insert into links (uri, link_type, updated_at, checked_at) values (?, ?, ?, ?)",
-                link.getUri().toString(),
-                link.getLinkType().toString(),
-                link.getUpdatedAt(),
-                link.getCheckedAt()
+                    "insert into links (uri, link_type, updated_at, checked_at) values (?, ?, ?, ?)",
+                    link.getUri()
+                            .toString(),
+                    link.getLinkType()
+                            .toString(),
+                    link.getUpdatedAt(),
+                    link.getCheckedAt()
             );
             linkId = findIdByUri(link.getUri());
         }
         return jdbcTemplate.update(
-            "insert into tg_chat_links(tg_chat_id, link_id) values (?, ?)",
-            tgChatId,
-            linkId.get()
+                "insert into tg_chat_links(tg_chat_id, link_id) values (?, ?)",
+                tgChatId,
+                linkId.get()
         );
     }
 
@@ -74,12 +78,13 @@ public class JdbcLinkRepository implements LinkRepository {
     @Transactional
     public List<Link> findAll() {
         List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from links");
-        List<Link> links = listMapToSetLink(list).stream().toList();
+        List<Link> links = listMapToSetLink(list).stream()
+                .toList();
         for (Link link : links) {
             List<Map<String, Object>> tgchatLinks = jdbcTemplate.queryForList(
-                "select tg_chats.id, tg_chats.chat_id from tg_chats join tg_chat_links"
-                    + " on tg_chats.id = tg_chat_links.tg_chat_id where link_id = ?",
-                link.getId()
+                    "select tg_chats.id, tg_chats.chat_id from tg_chats join tg_chat_links"
+                            + " on tg_chats.id = tg_chat_links.tg_chat_id where link_id = ?",
+                    link.getId()
             );
             Set<TgChat> tgChats = listMapToSetTgChat(tgchatLinks);
             link.setTgChats(tgChats);
@@ -90,7 +95,7 @@ public class JdbcLinkRepository implements LinkRepository {
     public Optional<Long> findIdByUri(URI uri) {
         try {
             Long linkId = jdbcTemplate.queryForObject("select id from links where uri = ?", Long.class,
-                uri.toString()
+                    uri.toString()
             );
             return Optional.of(linkId);
         } catch (EmptyResultDataAccessException e) {
@@ -101,18 +106,18 @@ public class JdbcLinkRepository implements LinkRepository {
     @Override
     public List<Link> findStaleLinks(Long limit) {
         List<Map<String, Object>> list =
-            jdbcTemplate.queryForList("select * from links order by checked_at asc limit ?", limit);
-        return listMapToSetLink(list).stream().toList();
+                jdbcTemplate.queryForList("select * from links order by checked_at asc limit ?", limit);
+        return listMapToSetLink(list).stream()
+                .toList();
     }
 
     @Override
     public int update(Long linkId, OffsetDateTime updatedAt, OffsetDateTime checkedAt) {
-
         return jdbcTemplate.update(
-            "update links set updated_at = ?, checked_at = ? where id = ?",
-            updatedAt,
-            checkedAt,
-            linkId
+                "update links set updated_at = ?, checked_at = ? where id = ?",
+                updatedAt,
+                checkedAt,
+                linkId
         );
     }
 }
