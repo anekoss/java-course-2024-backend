d47bad1883a41b78e05c365b1ec77f44962a23eb
Анна <ann1974chernova@gmail.com>
2024-04-08 16:20:54 +0300

    fix: update clients

--- scrapper/src/test/java/edu/java/scrapper/client/GitHubClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/client/GitHubClientTest.java
@@ -4,7 +4,7 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import edu.java.client.GitHubClient;
 import edu.java.client.dto.GitHubResponse;
-import edu.java.client.exception.BadResponseException;
+import edu.java.scrapper.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,21 +12,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.OffsetDateTime;
+import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class GitHubClientTest {
+public class GitHubClientTest extends IntegrationTest {
     @RegisterExtension
     static WireMockExtension wireMockServer = WireMockExtension.newInstance()
                                                                .options(wireMockConfig().dynamicPort())
@@ -42,7 +40,7 @@ public class GitHubClientTest {
     }
 
     @Test
-    void testGetRepository_shouldReturnCorrectResponse() throws IOException, BadResponseException {
+    void testGetRepository_shouldReturnCorrectResponse() throws IOException {
         String response = String.join("", Files.readAllLines(okResponsePath));
         wireMockServer.stubFor(WireMock.get("/repos/anekoss/tinkoff-project")
                                        .willReturn(aResponse().withStatus(200)
@@ -57,11 +55,13 @@ public class GitHubClientTest {
                 OffsetDateTime.parse("2024-02-21T12:54:35Z"),
                 OffsetDateTime.parse("2024-02-11T11:13:57Z")
         );
-        assertThat(gitHubClient.fetchRepository("anekoss", "tinkoff-project")).isEqualTo(excepted);
+        Optional<GitHubResponse> actual = gitHubClient.fetchRepository("anekoss", "tinkoff-project");
+        assert actual.isPresent();
+        assertEquals(actual.get(), excepted);
     }
 
     @Test
-    void testGetRepository_shouldReturnBadResponseExceptionIfClientError() throws IOException {
+    void testGetRepository_shouldReturnEmptyOptionalIfClientError() throws IOException {
         String response = String.join("", Files.readAllLines(badResponsePath));
         wireMockServer.stubFor(WireMock.get("/repos/anekoss/tinkoff-project")
                                        .willReturn(aResponse().withStatus(404)
@@ -71,15 +71,12 @@ public class GitHubClientTest {
                                                               )
                                                               .withBody(response))
         );
-        BadResponseException exception = assertThrows(
-                BadResponseException.class,
-                () -> gitHubClient.fetchRepository("anekoss", "tinkoff-project")
-        );
-        assertThat(exception.getMessage()).isEqualTo("Bad response was returned from the service");
+        Optional<GitHubResponse> actual = gitHubClient.fetchRepository("anekoss", "tinkoff-project");
+        assert actual.isEmpty();
     }
 
     @Test
-    void testGetRepository_shouldReturnServerException() throws IOException {
+    void testGetRepository_shouldReturnEmptyOptionalIfServerError() throws IOException {
         String response = String.join("", Files.readAllLines(badResponsePath));
         wireMockServer.stubFor(WireMock.get("/repos/anekoss/tinkoff-project")
                                        .willReturn(aResponse().withStatus(500)
@@ -89,15 +86,12 @@ public class GitHubClientTest {
                                                               )
                                                               .withBody(response))
         );
-        HttpServerErrorException exception = assertThrows(
-                HttpServerErrorException.class,
-                () -> gitHubClient.fetchRepository("anekoss", "tinkoff-project")
-        );
-        assertThat(exception.getMessage()).isEqualTo("500 INTERNAL_SERVER_ERROR");
+        Optional<GitHubResponse> actual = gitHubClient.fetchRepository("anekoss", "tinkoff-project");
+        assert actual.isEmpty();
     }
 
     @Test
-    void testGetRepository_shouldReturnBadResponseExceptionIfBadBody() {
+    void testGetRepository_shouldReturnEmptyOptionalIfBadBody() {
         wireMockServer.stubFor(WireMock.get("/repos/anekoss/tinkoff-project")
                                        .willReturn(aResponse().withStatus(200)
                                                               .withHeader(
@@ -105,9 +99,8 @@ public class GitHubClientTest {
                                                                       MediaType.APPLICATION_JSON_VALUE
                                                               )
                                                               .withBody("{id:mewmew}")));
-        BadResponseException exception = assertThrows(BadResponseException.class, () -> gitHubClient.fetchRepository("anekoss", "tinkoff-project"));
-        assertThat(exception.getMessage()).isEqualTo("Bad response was returned from the service");
+        Optional<GitHubResponse> actual = gitHubClient.fetchRepository("anekoss", "tinkoff-project");
+        assert actual.isEmpty();
     }
-
 
 }
