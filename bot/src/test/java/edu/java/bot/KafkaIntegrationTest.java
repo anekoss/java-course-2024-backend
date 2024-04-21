db4988a199d74647b5b30d60abbfdfe584e319c4
Анна <ann1974chernova@gmail.com>
2024-04-21 05:22:31 +0300

    tests: add kafka tests

---
+++ bot/src/test/java/edu/java/bot/KafkaIntegrationTest.java
@@ -0,0 +1,29 @@
+package edu.java.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+public abstract class KafkaIntegrationTest {
+
+    public static KafkaContainer KAFKA;
+
+    static {
+        KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
+        KAFKA.start();
+    }
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("app.kafka-config.bootstrap-servers", KAFKA::getBootstrapServers);
+    }
+    @Test
+    void test(){
+        assertTrue(true, "kafka container running");
+    }
+}
---
+++ bot/src/test/java/edu/java/bot/service/KafkaUpdateServiceTest.java
@@ -0,0 +1,78 @@
+package edu.java.bot.service;
+
+import edu.java.bot.KafkaIntegrationTest;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.codec.DecodingException;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class KafkaUpdateServiceTest extends KafkaIntegrationTest {
+
+    @MockBean
+    private UpdatesSenderService updatesSenderService;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    private KafkaUpdateService kafkaUpdateService;
+    @Value("${app.kafka-config.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${app.kafka-config.topic.name}")
+    private String topic;
+    private String dlq = "dlq";
+
+    private Map<String, Object> getConsumerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put("properties.spring.json.trusted.packages", "*");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return props;
+    }
+
+    @Test
+    void testUpdateService_shouldCorrectlyExecuteBot() throws InterruptedException {
+        doNothing().when(updatesSenderService).sendUpdates(any());
+        String linkUpdateRequest =
+            "{ \"id\": 1, \"url\": \"url\", \"description\": \"updated\", \"tgChatIds\": [1, 2] }";
+        kafkaTemplate.send(topic, linkUpdateRequest);
+        wait(10000);
+        verify(updatesSenderService, times(1)).sendUpdates(any());
+    }
+
+    @Test
+    void testUpdateService_shouldSendExceptionToDlqTopic() {
+        doThrow(DecodingException.class).when(updatesSenderService).sendUpdates(any());
+        String linkUpdateRequest =
+            "{ \"id\": 1, \"url\": \"url\", \"description\": \"updated\", \"tgChatIds\": [1, 2] }";
+        kafkaTemplate.send(topic, linkUpdateRequest);
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(getConsumerProps())) {
+            kafkaConsumer.subscribe(List.of(dlq));
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(10));
+            assert !records.isEmpty();
+            assert !records.iterator().next().value().isEmpty();
+
+        }
+    }
+
+}
